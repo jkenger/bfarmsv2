c993ef3a066842f557d45cc4ddffc31eaaec54f3
ken <johnkgervacio@gmail.com>
2023-12-01 10:16:44 +0800

    fixes

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -3,10 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 import axios from "axios";
 
-const localUrl =
-  import.meta.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api/v1"
-    : "/api/v1";
+const localUrl = "/api/v1";
 // const prodUrl = process.env.REACT_APP_API_URL;
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
