4d22fefe731e769ff691a2ee93f2e3a503179e26
ken <johnkgervacio@gmail.com>
2023-12-22 14:41:30 +0800

    fix: build error

--- client/src/components/ui/parse-time.tsx
+++ client/src/components/ui/parse-time.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import moment from "moment";
+
 type Props = {
   children: React.ReactNode;
 };
 
 function ParseTime({ children }: Props) {
-  return <span>{moment(String(children)).local().format("LTS")}</span>;
+  return (
+    <span>
+      {new Date(String(children)).toLocaleTimeString("en-PH", {
+        timeZone: "Asia/Manila",
+      })}
+    </span>
+  );
 }
 
 export default ParseTime;
