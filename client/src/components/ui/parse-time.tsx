41f68370d2ff04f9d483e5bfb9642b1e6834a341
ken <johnkgervacio@gmail.com>
2023-12-22 14:21:15 +0800

    fix test

--- client/src/components/ui/parse-time.tsx
+++ client/src/components/ui/parse-time.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import moment from "moment";
 type Props = {
   children: React.ReactNode;
 };
 
 function ParseTime({ children }: Props) {
-  return <span>{new Date(String(children)).toLocaleTimeString()}</span>;
+  return <span>{moment(String(children)).format("LTS")}</span>;
 }
 
 export default ParseTime;
