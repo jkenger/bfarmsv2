19764932750ecea9a278bd949a4958420718be8f
ken gervacio <johnkgervacio@gmail.com>
2024-03-28 23:23:31 +0800

    some fixes on cookies

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -91,6 +91,7 @@ export const loginStep2 = async (req, res) => {
     return res.status(401).json({
       message: "You are not authorized to perform login step-2",
       err: err,
+      step2Token,
     });
   }
 
